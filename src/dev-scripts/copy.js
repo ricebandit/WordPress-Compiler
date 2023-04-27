acd3931de1e2c958fe1357e6fabff073231b7d57
Jeremy Tani <jeremyt@homebrewagency.com>
2023-04-26 18:42:16 -0700

    initial build

---
+++ src/dev-scripts/copy.js
@@ -0,0 +1,35 @@
+const themeName = 'perfection';
+const fs = require('fs');
+const path = require('path');
+
+const copyRecursive = (src, dest) => {
+    const exists = fs.existsSync(src);
+    const stats = exists && fs.statSync(src);
+    const isDirectory = stats && stats.isDirectory();
+
+
+    if(isDirectory){
+        if(!fs.existsSync(dest)){
+            if( dest.indexOf('dev-scripts') == -1 ){
+                fs.mkdirSync(dest);
+            }else{
+                return;
+            }
+            
+        }
+
+        fs.readdirSync(src).forEach( childItemName => {
+            copyRecursive(path.join(src, childItemName), path.join(dest, childItemName));
+            
+        })
+    }else{
+        fs.copyFileSync(src, dest);
+    }
+}
+
+copyRecursive('./src', '../build/wp-content/themes/' + themeName);
+
+
+
+
+
